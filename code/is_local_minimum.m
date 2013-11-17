f5712e52baa4440e264f66214ddbb5369bf2d0da
ilya-yamschikov <ilya.yamschikov@gmail.com>
2013-11-18 02:23:44 +0400

    benchmark version =)

---
+++ code/is_local_minimum.m
@@ -0,0 +1,14 @@
+function result = is_local_minimum(img, point)
+    LOCAL_AREA_SIZE = 1;
+
+    if (point(1) <= LOCAL_AREA_SIZE) || (point(1) >= (size(img, 1) + 1 - LOCAL_AREA_SIZE)) || (point(2) <= LOCAL_AREA_SIZE) || (point(2) >= (size(img, 2) + 1 - LOCAL_AREA_SIZE))
+        result = false;
+        return;
+    end
+    
+    local_area = img((point(1) - LOCAL_AREA_SIZE):(point(1) + LOCAL_AREA_SIZE), (point(2) - LOCAL_AREA_SIZE):(point(2) + LOCAL_AREA_SIZE));
+    point_value = local_area(LOCAL_AREA_SIZE + 1,LOCAL_AREA_SIZE + 1);
+    local_area(LOCAL_AREA_SIZE + 1,LOCAL_AREA_SIZE + 1) = point_value + 1;
+    
+    result = all(local_area(:) > point_value);
+end
---
+++ code/multiple_blobs_detection_syntetic.m
@@ -0,0 +1,19 @@
+IMAGE_SIZE = 512;
+BLOBS_COUNT = 100;
+
+%start benchmark
+tStart = tic;
+
+[img centers radiuses] = generate_sample(IMAGE_SIZE, BLOBS_COUNT, 'blob');
+
+[detected_centers detected_radiuses] = detect_blobs(img);
+
+draw_image(img)
+hold on
+for i = 1:length(detected_centers)
+    plot(detected_centers{i}(2),detected_centers{i}(1), 'ro', 'MarkerSize',detected_radiuses{i}*8, 'LineStyle', 'none'); 
+end
+hold off
+
+tElapsed = toc(tStart);
+disp(['Running time: ' num2str(tElapsed) 'sec.']);
